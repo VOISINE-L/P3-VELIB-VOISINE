81b5f861761b45a953bdb3d36b38277074e92a20
Laurence VOISINE <laurence.voisine@gmail.com>
2019-11-24 16:59:06 +0100

    Second essai pour faire fonctionner le reservation Manager, Pb avec le e.preventDefault Reservation.js et "file not found" sur le submit

--- JS/app.js
+++ JS/app.js
@@ -99,17 +99,20 @@ class App {
           velos.textContent = "";
           adresse.textContent = "";
         }) // FERMETURE ADDLISTENERS MESSAGE FERMETURE
-      } /*else { //APPEL DU CANVAS SIGNATURE
-
-        this.submit.addEventListener("click", () => {
-          this.divForm.style.height = "460px";
+      } else { //APPEL DU CANVAS SIGNATURE
+/*
+      submit.addEventListener("click", (e) => {
+          e.preventDefault()
+        this.divForm.style.height = "460px";
           this.divForm.style.top = "calc(50% - 260px)"
           this.divResa.style.textAlign = "right"
           const firm = new Firm("div_resa");
           //marqueurCarte.removeEventListener("click")//? ne fonctionne pas
         })*/
 
-      }) //FERMETURE DU ELSE
-      //FERMETURE ADDEVENTLISTENERS SUR MARQUEURCARTE
-    }
+      }
+
+
+    })
   }
+}
--- JS/reservation.js
+++ JS/reservation.js
@@ -1,31 +1,30 @@
 class ReservationManager {
   constructor(reservation_box_id) {
-    this.reservation_box_id = reservation_box_id
+  this.reservation_box_id = reservation_box_id
     this.station = undefined;
     this.surname = undefined;
     this.name = undefined;
-    this.EndReservation = undefined;
-    this.firmBox = undefined; // ex divForm
-    this.firm = undefined;
-    this.userFormContainer = document.getElementById(reservation_box_id)
+    this.EndReservation = undefined; // heure de fin de  reservation
+    this.firmBox = undefined;//ex DivForm
+    this.firm = undefined;// signature
+    this.userFormContainer = document.getElementById(reservation_box_id)// ex divResa
     this.userForm = document.getElementById("form2") // ex form2
     this.addListener;
   }
   addListener() {
-      submit.addEventListener("click", function(e) {
-          e.preventDefault()
-
-          let surname = this.userForm.elements.prenom_utilisateur.value;
-          let name = this.userForm.elements.nom_utilisateur.value;
-
-          console.log("surname", surname)
-          console.log("name", name)
-          //this.divForm.style.height = "460px";
-          //this.divForm.style.top = "calc(50% - 260px)"
-          //this.divResa.style.textAlign = "right";
-          if ((surname.length > 0) && (name.length > 0)) {
-            this.firmBox = new Firm(this.reservation_box_id)
-          }
-        })
+      let submit = document.getElementById("submit")
+      submit.addEventListener("click",(e)=> {
+        e.preventDefault()
+      let name = this.userform.elements.prenom_utilisateur.value
+      let surname = this.userForm.elements.nom_utilisateur.value
+      console.log("surname", surname)
+      console.log("name", name)
+      if ( surname.length>0 && name.length>0){
+        this.firmBox = new Firm( this.reservation_box_id)
       }
-    }
+
+
+
+    })
+        }
+      }
